1651affa043679bbd4593f559b28415bab2309ce
Agung Prasetya <agungprasetya1121@gmail.com>
2022-03-15 18:45:59 +0700

    routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddEvent() {
+  return (
+    <div>
+      <h1>Hello dinamic</h1>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function add() {
+  return <div>add Event Page</div>;
+}
+
+export default add;
